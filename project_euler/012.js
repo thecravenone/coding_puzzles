6cd45f00285b46ce18e952ec4ef98d17f45582a0
Sam Craven <sam@seemsgood.com>
2015-11-11 19:26:43 -0600

    p_e/12 - Massive efficiency upgrade
    Consulted the oracle on this one and then worked it out by hand until I
    understood it.
    * Except for the number 1, all numbers start with 2 factors; the number
    and 1.
    * For each factor that is found, another factor exists.
    * Each factor below the square root of the targeted number will have a
    “mate” that is above the square root of the targeted number.
    * Therefore, start at 2, then search up to the square root of the
    number, incrementing by 2 when a factor is found.
    
    This change has also been applied to the number_of_factors include.

--- project_euler/012.js
+++ project_euler/012.js
@@ -1,30 +1,20 @@
 //Title: Project Euler problem - Highly divisible triangular number
 //Source: https://projecteuler.net/problem=12
 
-//Generate the xth triangle number
-/*
-var triangle_number = function(x) {
-	var sum =0;
-	for(var i=0; i<=x; i++) {
-		sum += i;
-	}
-	return sum
-};
-*/
-//That's great and all but I don't want to start from scratch on each triangle number
+//Generate the next triangle number
 var next_triangle_number = function(previous_triangle_number, previous_position) {
 	return previous_triangle_number + previous_position + 1;
 };
 
 var number_of_factors = function(x) {
-	var num_factors = 0;
+	var num_factors = 2;	//The number and 1. True for all natural numbers > 1
 	if(x == 1) {
 		num_factors = 1;
 		return num_factors;
 	}
-	for(var i=1; i<x/2+1; i++) {
+	for(var i=1; i<Math.floor(Math.sqrt(x)); i++) {
 		if (x % i == 0) {
-			num_factors++;
+			num_factors+=2;	//The factor found + the thing it gets multiplied by.
 		}
 	}
 	num_factors++;	//to account for the number itself.
